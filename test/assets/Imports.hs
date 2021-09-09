4d465e3bdc884406bf9287e092e7918254ed58ec
Stoeffel <schtoeffel@gmail.com>
2021-09-09 14:17:09 +0200

    Add some more variations of imports

--- test/assets/Imports.hs
+++ test/assets/Imports.hs
@@ -1,6 +1,14 @@
 module Imports where
 
 import NriPrelude ((>>))
+import Tuple (second)
+import qualified Tuple
+import Prelude hiding ((>>))
 
--- > (f >> f) x ==> f x
+-- > (f >> f) 1 ==> f 1
 f x = x
+
+--
+-- > second (True, False) ==> False
+--
+-- > Tuple.first (True, False) ==> True
