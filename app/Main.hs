e32e82324406a42649e3749242f0d34d81dccc66
Marcelle Rusu <marcelrusu@protonmail.com>
2021-10-19 11:38:59 -0400

    an attempt was made

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
 import qualified Control.Monad
@@ -9,28 +10,38 @@ import qualified System.Environment
 import System.FilePath.Find ((&&?), (==?))
 import qualified System.FilePath.Find as Find
 import qualified Prelude
+import System.Console.CmdArgs (def, args, modes, (&=), typ, auto, explicit, name, cmdArgsMode, cmdArgsRun, Typeable)
+import Data.Data (Data)
+
+data Sample = Sample { fileName :: [Prelude.FilePath] }
+              deriving (Prelude.Show, Data, Typeable)
+
+sample = cmdArgsMode (modes [Sample { fileName = def &= args }&= auto &= explicit])
+
 
 main :: Prelude.IO ()
 main = do
-  logHandler <- Platform.silentHandler
-  handler <- HVE.handler logHandler
-  cwd <- System.Directory.getCurrentDirectory
-  params <- System.Environment.getArgs
-  files <- case params of
-    [file] -> Prelude.pure [file]
-    [] -> Find.find (noRCS &&? noDist) (Find.extension ==? ".hs") cwd
-  results <-
-    files
-      |> List.map
-        ( \modulePath -> do
-            parsed <- HVE.parse handler modulePath
-            cradleInfo <- HVE.tryLoadImplicitCradle handler modulePath
-            results <- HVE.verify handler cradleInfo parsed
-            Task.succeed (HVE.moduleInfo parsed, results)
-        )
-      |> Task.parallel
-      |> Task.attempt logHandler
-  HVE.report [HVE.Stdout] results
+  result <- cmdArgsRun sample
+  Prelude.print (result)
+  -- logHandler <- Platform.silentHandler
+  -- handler <- HVE.handler logHandler
+  -- cwd <- System.Directory.getCurrentDirectory
+  -- params <- System.Environment.getArgs
+  -- files <- case params of
+  --   [file] -> Prelude.pure [file]
+  --   [] -> Find.find (noRCS &&? noDist) (Find.extension ==? ".hs") cwd
+  -- results <-
+  --   files
+  --     |> List.map
+  --       ( \modulePath -> do
+  --           parsed <- HVE.parse handler modulePath
+  --           cradleInfo <- HVE.tryLoadImplicitCradle handler modulePath
+  --           results <- HVE.verify handler cradleInfo parsed
+  --           Task.succeed (HVE.moduleInfo parsed, results)
+  --       )
+  --     |> Task.parallel
+  --     |> Task.attempt logHandler
+  -- HVE.report [HVE.Stdout] results
 
 noRCS :: Find.RecursionPredicate
 noRCS =
